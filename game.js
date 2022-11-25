63bcd256151daed67d8b7c678ab3bbd73bda53d2
Guilherme Azeredo Mendes <gui.a.mendes5@gmail.com>
2022-11-25 15:58:45 -0300

    Final Version

--- game.js
+++ game.js
@@ -1,7 +1,10 @@
 const question = document.getElementById('question');
 const choices = Array.from(document.getElementsByClassName("choice-text"));
-const questionCounterText = document.getElementById('questionCounter');
+const progressText = document.getElementById('progressText');
 const scoreText = document.getElementById('score');
+const progressBarFull = document.getElementById('progressBarFull');
+const loader = document.getElementById('loader');
+const game = document.getElementById('game');
 
 let currentQuestion = {};
 let acceptingAnswers = true;
@@ -9,32 +12,46 @@ let score = 0;
 let questionCounter = 0;
 let availableQuestions = [];
 
-let questions = [
-    {
-        question: "Inside which HTML element do we put the JavaScript?",
-        choice1: "<script>",
-        choice2: "<javascript>",
-        choice3: "<js>",
-        choice4: "<scipting>",
-        answer: 1
-    },
-    {
-        question: "What is the correct syntax for referring to an external script called 'xxx.js'?",
-        choice1: "<script href='xxx.js'>",
-        choice2: "<script name='xxx.js'>",
-        choice3: "<script src='xxx.js'>",
-        choice4: "<script file='xxx.js'>",
-        answer: 3
-    },
-    {
-        question: "How do you write 'Hello World' in an alert box?",
-        choice1: "msgBox('Hello World');",
-        choice2: "alertBox('Hello World');",
-        choice3: "msg('Hello World');",
-        choice4: "alert('Hello World');",
-        answer: 4
-    }
-]
+let questions = [];
+
+/*
+General Knnowledge
+https://opentdb.com/api.php?amount=10&category=9&difficulty=easy&type=multiple
+
+Entertainment: Video Games
+https://opentdb.com/api.php?amount=10&category=15&type=multiple
+*/
+
+fetch("https://opentdb.com/api.php?amount=10&category=15&type=multiple")
+    .then(res => {
+        //console.log(res);
+        return res.json();
+    })
+    .then(loadedQuestions => {
+        console.log(loadedQuestions.results);
+        questions = loadedQuestions.results.map(loadedQuestion => {
+            const formattedQuestion = {
+                question: loadedQuestion.question
+            };
+
+            const answerChoices = [...loadedQuestion.incorrect_answers];
+            formattedQuestion.answer = Math.floor(Math.random() * 3) + 1;
+            answerChoices.splice(formattedQuestion.answer -1, 0, loadedQuestion.correct_answer);
+
+            answerChoices.forEach((choice, index) => {
+                formattedQuestion["choice" + (index + 1)] = choice;
+            });
+
+            return formattedQuestion;
+        });
+        //questions = loadedQuestions;
+
+        //Start Game
+        startGame();
+    })
+    .catch( err => {
+    console.log(err);
+})
 
 //CONSTANTS
 const CORRECT_BONUS = 10;
@@ -46,25 +63,31 @@ startGame = () => {
     availableQuestions = [ ...questions];
 
     getNewQuestion();
+
+    game.classList.remove("hidden");
+    loader.classList.add("hidden");
 };
 
 getNewQuestion = () => {
 
     if(availableQuestions.length === 0 || questionCounter >= MAX_QUESTIONS){
+        localStorage.setItem('mostRecentScore', score);
         //go to the end page
         return window.location.assign("/end.html");
     }
 
     questionCounter++;
-    questionCounterText.innerText = `${questionCounter}/${MAX_QUESTIONS}`;
+    progressText.innerText = `Question ${questionCounter}/${MAX_QUESTIONS}`;
+    //Update the progress bar
+    progressBarFull.style.width = `${(questionCounter / MAX_QUESTIONS) * 100}%`;
 
     const questionIndex = Math.floor(Math.random() * availableQuestions.length);
     currentQuestion = availableQuestions[questionIndex];
-    question.innerText = currentQuestion.question;
+    question.innerHTML = currentQuestion.question;
 
     choices.forEach(choice => {
         const number = choice.dataset['number'];
-        choice.innerText = currentQuestion['choice' + number]
+        choice.innerHTML = currentQuestion['choice' + number]
     });
 
     availableQuestions.splice(questionIndex, 1);
@@ -99,6 +122,4 @@ choices.forEach(choice => {
 incrementScore = num => {
     score += num;
     scoreText.innerText = score;
-}
-
-startGame();
+};
---
+++ highscores.js
@@ -0,0 +1,11 @@
+const highScoresList = document.getElementById('highScoresList');
+const highScores = JSON.parse(localStorage.getItem("highScores")) || [];
+
+//console.log(highScores);
+
+highScoresList.innerHTML =
+    highScores.map(score => {
+        //console.log(`<li class="high-score">${score.name}-${score.score}</li>`);
+        return `<li class="high-score">${score.name} - ${score.score}</li>`;
+    })
+    .join("");
